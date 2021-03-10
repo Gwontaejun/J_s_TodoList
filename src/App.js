6f4e807f7f1587f4da01e23f2373e48c16940cca
Gwontaejun <hope10204@daum.net>
2021-03-11 04:47:51 +0900

    2021-03-11 TodoList 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, Component } from 'react';
+import Media from 'react-media';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const REACT_MEDIA_QUERIES = {
+  PC: "(min-width:1024px)",
+  TABLET: "(min-width:768px) and (max-width:1023px)",
+  MOBILE: "(max-width:767px)"
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Media queries={REACT_MEDIA_QUERIES}>
+          {matches => {
+            return(
+              <div>
+                {matches.PC && <div>PC전용화면</div>}
+                {matches.TABLET && <div>타블렛전용화면</div>}
+                {matches.MOBILE && <div>모바일전용화면</div>}
+              </div>
+            )
+          }}
+        </Media>
+      </div>
+    )
+  }
 }
 
 export default App;
